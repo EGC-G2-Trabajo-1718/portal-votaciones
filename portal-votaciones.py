6e92bd70ff9db14c0b2982b6193db84a9076e47d
MOhammed <mohammed.alaoui_mhammedi@insa-rouen.fr>
2018-01-10 10:41:53 +0100

    feat:añadir fichero para evitar conflicto

--- portal-votaciones.py
+++ portal-votaciones.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+
+    return render_template('index.html')
+
+
+@app.route('/iniciar')
+def iniciar_session():
+
+    return render_template('iniciar_session/autentificacion.html')
+
+@app.route('/visualizacion')
+def visualizacion():
+
+    return render_template('visualizacion_resultados/index.html')
 
 
 if __name__ == '__main__':
