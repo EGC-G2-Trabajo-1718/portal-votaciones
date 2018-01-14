dde6962413876eb606f84b7725416626259fa330
Belen <belramgut@alum.us.es>
2018-01-14 17:40:14 +0100

    fix(inicio app): Arreglos en general en la aplicación

--- portal-votaciones.py
+++ portal-votaciones.py
@@ -47,38 +47,11 @@ def hourChart():
     return render_template('hourChart.html',tramos=tramos, votos=votos)
 
 
-@app.route('/graficaEdad')
-def graficaedad():
-    rango,votos = parsingDeFicheros.parseEdades()
-    return render_template('graficaEdad.html',edades=rango, recuento=votos)
-
-@app.route('/graficaCiudad')
-def graficaciudad():
-    ciudades,votos = parsingDeFicheros.parseLugaresGeograficos("ciudades")
-    return render_template('graficaCiudad.html',ciudades=ciudades, votos=votos)
-
 @app.route('/graficaPaises')
 def graficaPaises():
     paises,votos = parsingDeFicheros.parseLugaresGeograficos("paises")
     return render_template('graficaPais.html',paises=paises, votos=votos)
 
-
-@app.route('/graficaResultados')
-def graficaResultados():
-    opciones,votos = parsingDeFicheros.votosPorOpcionNuevaApi(None,None)
-    return render_template('graficaResultados.html',opciones=opciones, votos=votos)
-
-@app.route('/graficaHoras')
-def graficaHoras():
-    tramos,votos = parsingDeFicheros.votosPorTramoHorario()
-    return render_template('graficaHoras.html',tramos=tramos, votos=votos)
-
-@app.route('/grafica_censo_votos')
-def graficaCensoYVotos():
-    censo = parsingDeFicheros.obtener_censo(None,None)
-    votos = parsingDeFicheros.obtener_votantes(None,None)
-    censo_y_votos = [censo,votos]
-    return render_template('votosPorCenso.html',censo_y_votos=censo_y_votos)
 
 
 if __name__ == '__main__':
